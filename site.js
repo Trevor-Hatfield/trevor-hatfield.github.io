41ef7aa7b4d03135f978b568b16412a6be2be79f
HatfieldT <800072352@fvtc.edu>
2025-08-31 14:12:14 -0500

    Added welcome div

---
+++ site.js
@@ -0,0 +1,22 @@
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+// Add a div element named 'welcome' to the header element to display above the nav bar
+// Determine what condition to run at different times of the day for welcome message
+const welcome = document.querySelector('#welcome');
+
+    if (isMorning) 
+    {
+        welcome.textContent = 'Good Morning'
+    }
+    else if (isAfternoon) 
+    {
+        welcome.textContent = 'Good Afternoon'
+    }
+    else 
+    {
+        welcome.textContent = 'Good Evening'
+    }
