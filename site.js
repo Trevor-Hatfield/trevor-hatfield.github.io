0e37b5815ae30f4d428bdb07dd23daf9828c4530
HatfieldT <800072352@fvtc.edu>
2025-09-15 22:38:27 -0500

    Added carousel

--- site.js
+++ site.js
@@ -24,3 +24,47 @@ const welcome = document.querySelector('#welcome');
 
 localStorage.setItem(`It's a secret to everybody.`, `You need a couple more heart containers before facing Gannon`)
 
+// Carousel Assignment
+
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+// add event listener for previous and next buttons.
+// each click should increment/decrement current image then run show images function 
+
+const next = document.querySelector('#next')
+next.addEventListener('click', ()=> {
+    currentImage++
+    showImages(currentImage)
+})
+
+const prev = document.querySelector('#prev')
+prev.addEventListener('click', ()=> {
+    currentImage--
+    showImages(currentImage)
+})
+
+// Add interval timer to cycle to next image automatically every 5 seconds
+
+setInterval(cycleImages = () => {
+    currentImage++
+    showImages(currentImage)
+}, 5000)
